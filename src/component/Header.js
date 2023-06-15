baa0cf1c39df7804567566913a6ba402bcb1bca8
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-06-16 00:19:52 +0600

    ok

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-        headrf
+    <div style={{
+        height:"65px",
+        width:'95%',
+        marginTop:'5px',
+        marginLeft:'2.5%',
+        borderRadius:'10px',
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center',
+    }}>
+        <h2>Task Management System </h2>
+        <div>ghjkjvhj</div>
     </div>
   )
 }
