fb1a7329538e83768ac755f1579719e0a20e4623
Tasnim Shahriar Akash <tasnim15-2606@diu.edu.bd>
2023-06-16 00:26:46 +0600

    ok

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React from "react";
+import { LoginOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div style={{
-        height:"65px",
-        width:'95%',
-        marginTop:'5px',
-        marginLeft:'2.5%',
-        borderRadius:'10px',
-        display:'flex',
-        flexDirection:'row',
-        justifyContent:'center',
-        alignItems:'center',
-    }}>
-        <h2>Task Management System </h2>
-        <div>ghjkjvhj</div>
+    <div
+      style={{
+        height: "65px",
+        width: "95%",
+        marginTop: "5px",
+        marginLeft: "2.5%",
+        borderRadius: "10px",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h2 style={{
+        flex:'10'
+      }} >Task Management System </h2>
+      <Button htmlType="button" style={{
+        flex:'.1'
+      }}>
+        <Link to="/LogIn">
+          {" "}
+          <LoginOutlined />
+        </Link>
+      </Button>
+      
     </div>
-  )
+  );
 }
